e3f30b425dba41f27e222de958490a601c08b51e
hanmin <minyo3746@gmail.com>
2020-04-27 09:40:30 +0900

    add

---
+++ OperationPromotionExample.java
@@ -0,0 +1,25 @@
+public class OperationsPromotionExample {
+	public static void main(String[] args) {
+		byte byteValue1 = 10;
+		byte byteValue2 = 20;
+		//byte byteValue3 = byteValue1 + byteValue2; 
+		int intValue1 = byteValue1 + byteValue2;
+		System.out.println(intValue1);
+		
+		char charValue1 = 'A';
+		char charValue2 = 1;
+		//char charValue3 = charValue1 + charValue2;
+		int intValue2 = charValue1 + charValue2;
+		System.out.println("unicode=" + intValue2);
+		System.out.println("print=" + (char)intValue2);
+
+		int intValue3 = 10;
+		int	intValue4 = intValue3/4;
+		System.out.println(intValue4);
+		
+		int intValue5 = 10;
+		//int intValue6 = 10 / 40;
+		double doubleValue = intValue5 / 40
+		System.out.println(doubleValue);
+	}
+}	
